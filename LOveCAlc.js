a072108c4a74a11f5b66be866dd34436ea658167
Manu <129163095+YamiMax252@users.noreply.github.com>
2023-04-19 15:42:53 +0530

    Create LOveCAlc.js

---
+++ LOveCAlc.js
@@ -0,0 +1,8 @@
+//love calc
+
+let n=prompt("what is your name");
+let m=prompt("what is your crush's name");
+let love=Math.random()*100;
+  love=Math.floor(love)+1;
+
+alert(n+" "+ m +' love score' +love +"%");
